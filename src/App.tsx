8af57313ab8e74d539deed6ede921f9ae1a23e49
Rajesh Nitharwal <rajesh.nitharwal@emails.emizentech.com>
2025-12-23 10:21:08 +0530

    Fix double Router issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layout/MainLayout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Experience from './pages/Experience';
-import Portfolio from './pages/Portfolio';
-import CodingSkills from './pages/CodingSkills';
-import Awards from './pages/Awards';
-import Contact from './pages/Contact';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Experience from "./pages/Experience";
+import Portfolio from "./pages/Portfolio";
+import CodingSkills from "./pages/CodingSkills";
+import Awards from "./pages/Awards";
+import Contact from "./pages/Contact";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="experience" element={<Experience />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="coding-skills" element={<CodingSkills />} />
-          <Route path="awards" element={<Awards />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="experience" element={<Experience />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="coding-skills" element={<CodingSkills />} />
+        <Route path="awards" element={<Awards />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
